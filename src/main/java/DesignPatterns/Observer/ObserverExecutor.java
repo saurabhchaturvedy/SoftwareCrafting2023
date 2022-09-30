ea6a4828300c1e73cf36e5fd6dd5817d6ab53042
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2022-09-30 21:14:56 +0530

    Implementing cricket score board updates

--- src/main/java/DesignPatterns/Observer/ObserverExecutor.java
+++ src/main/java/DesignPatterns/Observer/ObserverExecutor.java
@@ -7,13 +7,14 @@ public class ObserverExecutor {
     public static void main(String[] args) {
         CricketScore cricketScore = new CricketScore();
 
-        new Subscriber1(cricketScore);
-        new Subscriber2(cricketScore);
-        new Subscriber3(cricketScore);
+        Subscriber1 subscriber1 = new Subscriber1(cricketScore);
+        Subscriber2 subscriber2 = new Subscriber2(cricketScore);
+        Subscriber3 subscriber3 = new Subscriber3(cricketScore);
 
         ScoreBoard scoreBoard = new ScoreBoard();
         scoreBoard.setRuns(0);
         scoreBoard.setWickets(0);
+        cricketScore.removeObserver(subscriber2);
         cricketScore.setScoreBoard(scoreBoard);
         Scanner scanner = new Scanner(System.in);
         while (true) {
